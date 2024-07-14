c1028a7752096f5ecefa45e8380e2801476e41fb
João Guilherme <jaaumgui@gmail.com>
2024-07-14 20:59:31 -0300

    feat(CoordSaver): registering all commands

--- src/main/java/dev/joao_guilherme/coordSaver/CoordSaver.java
+++ src/main/java/dev/joao_guilherme/coordSaver/CoordSaver.java
@@ -1,5 +1,10 @@
 package dev.joao_guilherme.coordSaver;
 
+import dev.joao_guilherme.coordSaver.commands.GetLocationCommand;
+import dev.joao_guilherme.coordSaver.commands.ListLocationCommand;
+import dev.joao_guilherme.coordSaver.commands.RemoveLocationCommand;
+import dev.joao_guilherme.coordSaver.commands.SaveLocationCommand;
+import dev.joao_guilherme.coordSaver.utils.LocationsUtils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
@@ -9,6 +14,11 @@ public final class CoordSaver extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        LocationsUtils.setPlugin(this);
+        getCommand("coords").setExecutor(new ListLocationCommand());
+        getCommand("savecoord").setExecutor(new SaveLocationCommand());
+        getCommand("removecoord").setExecutor(new RemoveLocationCommand());
+        getCommand("getcoord").setExecutor(new GetLocationCommand());
         Bukkit.getConsoleSender().sendMessage(Component.text("[CoordSaver] Plugin initialized!").color(NamedTextColor.GREEN));
     }
 
