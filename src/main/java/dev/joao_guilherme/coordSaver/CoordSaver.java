439449e2bdeb5a9df03b3147399792cabb4cec5f
João Guilherme <jaaumgui@gmail.com>
2024-07-14 22:16:56 -0300

    feat(CoordSaver): Using Objects.requireNonNull method to remove warnings

--- src/main/java/dev/joao_guilherme/coordSaver/CoordSaver.java
+++ src/main/java/dev/joao_guilherme/coordSaver/CoordSaver.java
@@ -10,15 +10,17 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class CoordSaver extends JavaPlugin {
 
     @Override
     public void onEnable() {
         LocationsUtils.setPlugin(this);
-        getCommand("coords").setExecutor(new ListLocationCommand());
-        getCommand("savecoord").setExecutor(new SaveLocationCommand());
-        getCommand("removecoord").setExecutor(new RemoveLocationCommand());
-        getCommand("getcoord").setExecutor(new GetLocationCommand());
+        Objects.requireNonNull(getCommand("coords")).setExecutor(new ListLocationCommand());
+        Objects.requireNonNull(getCommand("savecoord")).setExecutor(new SaveLocationCommand());
+        Objects.requireNonNull(getCommand("removecoord")).setExecutor(new RemoveLocationCommand());
+        Objects.requireNonNull(getCommand("getcoord")).setExecutor(new GetLocationCommand());
         Bukkit.getConsoleSender().sendMessage(Component.text("[CoordSaver] Plugin initialized!").color(NamedTextColor.GREEN));
     }
 
